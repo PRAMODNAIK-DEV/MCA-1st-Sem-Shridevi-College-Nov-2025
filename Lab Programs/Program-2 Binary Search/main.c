e54bd7407e8f611d952b2451d10f1cd33b9cc989
PRAMODNAIK-DEV <pramodnaik.dev@gmail.com>
2025-12-19 00:28:03 +0530

    Completed Program-2 Binary Search

---
+++ Lab Programs/Program-2 Binary Search/main.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int size, int key) {
+    int low = 0, high = size - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;              // Element found
+        else if (arr[mid] < key)
+            low = mid + 1;           // Search right half
+        else
+            high = mid - 1;          // Search left half
+    }
+
+    return -1;   // -1 Means Element not found
+}
+
+int main() {
+    int arr[100], n, key, i;
+
+    printf("Enter number of elements: ");
+    scanf("%d", &n);
+
+    printf("Enter %d sorted elements:\n", n);
+    for (i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    printf("Enter element to search: ");
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        printf("Element found at position %d\n", result + 1);
+    else
+        printf("Element not found\n");
+
+    return 0;
+}
